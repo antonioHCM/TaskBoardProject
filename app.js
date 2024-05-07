1c764bcef8484bbe54a1e28c3af3bee7fc32eacb
antonioHCM <antoniohmagalhaes@hotmail.com>
2024-05-07 15:32:23 +0200

    swagger implemented

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mongoose = require ("mongoose");
 const bodyParser = require ("body-parser");
 const app = express();
 
+//swagger
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+
+const swaggerDefinition = yaml.load('./swagger.yaml');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
+
 //Cors handle
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
